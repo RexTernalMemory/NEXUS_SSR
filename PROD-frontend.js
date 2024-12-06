75ecd077b5d31b2e633549b37541e424e2fc8dc9
Rex Egonio Jr. <rex.egonio@cepatkredit.com>
2024-12-06 13:25:33 +0800

    Prod Setup

--- PROD-frontend.js
+++ PROD-frontend.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import compression from 'compression'
+import https from 'https'
 import fs from 'fs'
 import axios from 'axios'
 import { upload } from './FileAuth.js'
@@ -22,17 +23,15 @@ app.use(cors({
     ],
 }))
 
-app.get('/api/v1/GET/:COMMAND/:A?/:B?/:C?/:D?/:E?', async (req, res) => {
+app.get('/GET/:COMMAND/:A?/:B?/:C?/:D?/:E?', async (req, res) => {
     const { URL } = process.env;
-    let path = `${URL}/api/v1/GET/${req.params.COMMAND}`
+    let path = `${URL}/GET/${req.params.COMMAND}`
 
     if (req.params.A) { path += `/${req.params.A}` }
     if (req.params.B) { path += `/${req.params.B}` }
     if (req.params.C) { path += `/${req.params.C}` }
     if (req.params.D) { path += `/${req.params.D}` }
     if (req.params.E) { path += `/${req.params.E}` }
-
-    console.log(path)
 
     await axios.get(path)
         .then((result) => {
@@ -47,13 +46,13 @@ app.get('/api/v1/GET/:COMMAND/:A?/:B?/:C?/:D?/:E?', async (req, res) => {
         });
 });
 
-app.post('/api/v1/POST/:COMMAND/:A?/:B?/:C?/:D?/:E?', async (req, res, next) => {
+app.post('/POST/:COMMAND/:A?/:B?/:C?/:D?/:E?', async (req, res, next) => {
     const uploadRequiredAPIs = ['P147UFRL', 'P66UFR', 'P67UFF'];
     if (uploadRequiredAPIs.includes(req.params.COMMAND)) { return upload.array('files')(req, res, next); }
     next();
 }, async (req, res) => {
     const { URL } = process.env;
-    let path = `${URL}/api/v1/POST/${req.params.COMMAND}`
+    let path = `${URL}/POST/${req.params.COMMAND}`
 
     if (req.params.A) { path += `/${req.params.A}` }
     if (req.params.B) { path += `/${req.params.B}` }
@@ -155,4 +154,12 @@ app.get('/test', (req, res) => {
 })
 
 const { PORT } = process.env;
-app.listen(PORT, () => console.log(`Server Status: Listening on port ${PORT}`))
+//app.listen(PORT, () => console.log(`Server Status: Listening on port ${PORT}`))
+
+const options = {
+    key: fs.readFileSync("ckfi.key"),
+    cert: fs.readFileSync("STAR_ckfi_live.crt"),
+};
+https.createServer(options, app).listen(PORT, function (req, res) {
+    console.log(`RUNNING: ${PORT}`);
+});
